1f72320dc7bf722ceb47d387d0368f617c63005b
SeanParaiso <seanharvey.paraiso.cics@ust.edu.ph>
2023-11-23 01:56:33 +0800

    Complete 4 designs except navigation page. Added Database for login and signup

--- app/src/main/java/com/example/tracetag/MainActivity.kt
+++ app/src/main/java/com/example/tracetag/MainActivity.kt
@@ -1,11 +1,58 @@
 package com.example.tracetag
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.tracetag.R
+import com.example.tracetag.HomeActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private val sharedPrefFile = "user_prefs"
+    private lateinit var sharedPreferences: SharedPreferences
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.home)
+        setContentView(R.layout.login)
+
+        sharedPreferences = getSharedPreferences(sharedPrefFile, Context.MODE_PRIVATE)
+
+        val btnLogin = findViewById<Button>(R.id.btnLogin)
+        val etUsername = findViewById<EditText>(R.id.etLoginUsername)
+        val etPassword = findViewById<EditText>(R.id.etLoginPassword)
+        val btnSignup = findViewById<Button>(R.id.btnSignup) // Add this line
+
+        btnLogin.setOnClickListener {
+            val username = etUsername.text.toString()
+            val password = etPassword.text.toString()
+
+            // Create an instance of the DatabaseHandler
+            val dbHandler = DatabaseHandler(this)
+
+            // Use the loginUser function for authentication
+            val user = dbHandler.loginUser(username, password)
+
+            if (user != null) {
+                // Login successful
+                // Redirect to the next activity or perform other actions
+                Toast.makeText(this, "Login successful", Toast.LENGTH_SHORT).show()
+                val intent = Intent(this, HomeActivity::class.java)
+                startActivity(intent)
+                finish()
+            } else {
+                // Login failed
+                Toast.makeText(this, "Invalid username or password", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        btnSignup.setOnClickListener {
+            val intent = Intent(this, SignupActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
