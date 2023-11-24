fff82809303a2d00f6da41582122488b28f58e1e
iyaqn <maryjulia.malagayo.cics@ust.edu.ph>
2023-11-24 08:39:15 +0800

    home, item, profile, add found/lost item, clicking add button + top and bottom navbar na hindi pa naaattach sa mga pages T_T (baka may mga baguhin pa ako)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.tracetag"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.tracetag"
@@ -36,7 +36,7 @@ android {
 }
 
 dependencies {
-
+    implementation ("com.google.android.material:material:1.11.0-alpha01")
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
