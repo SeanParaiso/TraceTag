6509f547cd9f6b92c529b1c84217c3def3ee8521
iyaqn <maryjulia.malagayo.cics@ust.edu.ph>
2023-11-27 04:45:01 +0800

    added top and bottom navigation bars, made add button switch pages, added scroll views to pages

--- app/src/main/java/com/example/tracetag/MainActivity.kt
+++ app/src/main/java/com/example/tracetag/MainActivity.kt
@@ -1,11 +1,49 @@
 package com.example.tracetag
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Dialog
+import android.content.Intent
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+import android.view.Window
+import android.widget.ImageButton
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.home)
+        val addbtn = findViewById<ImageButton>(R.id.addbtn)
+        addbtn.setOnClickListener {
+            val message = ""
+            showAddbtn(message)
+        }
     }
+
+
+    fun showAddbtn(message: String){
+        val dialog = Dialog(this)
+
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        dialog.setCancelable(false)
+        dialog.setContentView(R.layout.clicking_add_button)
+        dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        dialog.show()
+
+        val addLostBtn = dialog.findViewById<ImageButton>(R.id.btnAddLost)
+        val addFoundBtn = dialog.findViewById<ImageButton>(R.id.btnAddFound)
+
+        addLostBtn.setOnClickListener {
+            val i = Intent(this, AddLostItem::class.java)
+            startActivity(i)
+        }
+        addFoundBtn.setOnClickListener {
+            val i = Intent(this, AddFoundItem::class.java)
+            startActivity(i)
+        }
+    }
+
+
+
 }
